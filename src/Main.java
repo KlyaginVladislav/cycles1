3150af4dddda75eb71c4e6a5bdf189bb756ac2fa
Vladislav <klyaginvladislav@mail.ru>
2022-05-27 00:22:21 +0300

    циклы, задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int currentNum = 0;
+        while (currentNum < 10){
+            System.out.println(  ++ currentNum + "цифра");
+        }
+        System.out.println();
+
+        for (;currentNum > 0; currentNum--){
+            System.out.println(currentNum + "цифра");
+        }
     }
 }
